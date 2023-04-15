39a468e8d92f34ae58f93b3b496fff68d2ebd26e
marcopigna <mpigna23@gmail.com>
2023-04-15 12:06:56 -0400

    Pictures showing up in all views now. Need to be defined with android:src=" "

--- app/src/main/java/com/example/rucafe_android/MainActivity.java
+++ app/src/main/java/com/example/rucafe_android/MainActivity.java
@@ -9,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_view);
-        //setContentView(R.layout.ordering_donuts_view);
+        //setContentView(R.layout.main_view);
+        setContentView(R.layout.ordering_donuts_view);
+        //setContentView(R.layout.ordering_basket_view);
     }
 }
