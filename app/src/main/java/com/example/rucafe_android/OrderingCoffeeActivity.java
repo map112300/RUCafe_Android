1db728f3944165c1b3186086611d665bac954e56
Bryan <Bryanrbezerra@gmail.com>
2023-04-25 02:07:41 -0400

    Activity titles

--- app/src/main/java/com/example/rucafe_android/OrderingCoffeeActivity.java
+++ app/src/main/java/com/example/rucafe_android/OrderingCoffeeActivity.java
@@ -2,13 +2,24 @@ package com.example.rucafe_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import android.os.Bundle;
 
 public class OrderingCoffeeActivity extends AppCompatActivity {
+    private Spinner sizeSpinner;
+    private ArrayAdapter<CharSequence> spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordering_coffee);
+        this.sizeSpinner = findViewById(R.id.coffee_size_spinner);
+        this.spinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.coffee_sizes, android.R.layout.simple_spinner_dropdown_item
+        );
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sizeSpinner.setAdapter(spinnerAdapter);
     }
 }
