46b0097ec76676cd1bfa63569e34cf515b68a66b
Bryan <Bryanrbezerra@gmail.com>
2023-04-26 20:05:28 -0400

    Remove orders from history

--- app/src/main/java/com/example/rucafe_android/StoreOrdersActivity.java
+++ app/src/main/java/com/example/rucafe_android/StoreOrdersActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,6 +20,7 @@ public class StoreOrdersActivity extends AppCompatActivity implements AdapterVie
     private ListView listView;
     private Spinner orderNumSpinner;
     private TextView totalSum;
+    private Button removeOrderButton;
     private ObservableArrayList<MenuItem> menuItemsInOrder;
     private ObservableArrayList<CharSequence> orderNums;
     private ArrayAdapter<CharSequence> orderNumAdapter;
@@ -28,9 +30,11 @@ public class StoreOrdersActivity extends AppCompatActivity implements AdapterVie
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_orders);
+        this.removeOrderButton = findViewById(R.id.order_history_remove_button);
         this.listView = findViewById(R.id.order_history_items);
         this.orderNumSpinner = findViewById(R.id.order_history_spinner);
         this.totalSum = findViewById(R.id.order_histroy_total);
+        createRemoveOrderButton(removeOrderButton);
         if (MainActivity.placedOrders.size() > 0) {
             this.currentOrder = MainActivity.placedOrders.get(0);
 
@@ -53,8 +57,24 @@ public class StoreOrdersActivity extends AppCompatActivity implements AdapterVie
                     R.layout.basket_list, R.id.list_content, menuItemsInOrder
             );
             listView.setAdapter(menuItemArrayAdapter);
-            setTotalPrice();
+            updateTotalPrice();
         }
+    }
+
+    private void createRemoveOrderButton(Button button) {
+        button.setOnClickListener(v -> {
+            MainActivity.placedOrders.remove(this.currentOrder);
+            this.currentOrder = null;
+            if (MainActivity.placedOrders.size() > 0) {
+                this.currentOrder = MainActivity.placedOrders.get(0);
+                updateItemList();
+            } else {
+                menuItemsInOrder.clear();
+                menuItemArrayAdapter.notifyDataSetChanged();
+                updateTotalPrice();
+            }
+            updateOrderNumList();
+        });
     }
 
     @Override
@@ -78,11 +98,24 @@ public class StoreOrdersActivity extends AppCompatActivity implements AdapterVie
         itemArray = currentOrder.getMenuItems().toArray(itemArray);
         Collections.addAll(menuItemsInOrder, itemArray);
         menuItemArrayAdapter.notifyDataSetChanged();
-        setTotalPrice();
+        updateTotalPrice();
     }
 
-    private void setTotalPrice() {
-        Double total = currentOrder.getOrderSubtotal() * NJ_TAX_MULTIPLIER;
+    private void updateOrderNumList() {
+        this.orderNums.clear();
+        for (Order order : MainActivity.placedOrders) {
+            orderNums.add(Integer.toString(order.getOrderNumber()));
+        }
+        this.orderNumAdapter.notifyDataSetChanged();
+    }
+
+    private void updateTotalPrice() {
+        Double total;
+        if (this.currentOrder != null) {
+            total = currentOrder.getOrderSubtotal() * NJ_TAX_MULTIPLIER;
+        } else {
+            total = 0.0;
+        }
         String totalString = String.format("%.2f", total);
         totalSum.setText(totalString);
     }
